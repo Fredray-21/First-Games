9097b72b232aea18b8567ed88ff0ae3f56ba9e83
Frédéric Dabadie <dabadie.frederic@gmail.com>
2022-04-24 00:27:18 +0200

    add message victoire/defaite

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ start();
 async function start() {
     const mot = await randomWord();
     console.log(mot);
-    let etapependu = 0;
+    let etapependu = 5;
     etapependu == 5 ? $("#bar-bottom,#bar-vertical,#bar-top,#bar-corner,#bar-corde").css("display", "block") : false;
     mot.split("").forEach((e) => {
         $("#row-align-bottom").append("<div class='letter' data-id='" + e.toLocaleUpperCase() + "'></div>");
@@ -16,12 +16,12 @@ async function start() {
         });
         $(this).unbind("click");
         $(this).css("cursor", "not-allowed");
-        good ? $(this).css("background-color", "green") : ($(this).css("opacity", "0.5"), $(this).css("background-color", "lightcoral"), etapependu++, pendu(etapependu));
+        good ? ($(this).css("background-color", "green"), isWin(mot)) : ($(this).css("opacity", "0.5"), $(this).css("background-color", "lightcoral"), etapependu++, pendu(etapependu,mot));
     });
 }
 
 
-function pendu(etapependu) {
+function pendu(etapependu,mot) {
     switch (etapependu) {
         case 1: $("#bar-bottom").css("display", "block"); break;
         case 2: $("#bar-vertical").css("display", "block"); break;
@@ -33,10 +33,38 @@ function pendu(etapependu) {
         case 8: $("#bar-armL").css("display", "block"); break;
         case 9: $("#bar-armR").css("display", "block"); break;
         case 10: $("#bar-legL").css("display", "block"); break;
-        case 11: $("#bar-legR").css("display", "block"); break;
+        case 11: $("#bar-legR").css("display", "block"); isDefeat(mot); break;
     }
 }
 
+function isWin(mot) {
+    let win = true;
+    $("#row-align-bottom .letter").each(function () {
+        $(this).text() == "" ? win = false : false;
+    });
+    if (win) {
+        $("#row-top").css("flex-direction", "column");
+        $("#row-top").css("align-items", "center");
+        $("#row-top").css("padding-bottom", "0");
+        $("#row-top").html("<div class='winner title'>Victoire</div>");
+        $("#row-top").append("<div class='winner'>(" + mot + ")</div>");
+
+        $(".key").css("opacity", "0.5");
+        $(".key").unbind("click");
+        $(".key").css("cursor", "not-allowed");
+    }
+}
+function isDefeat(mot) {
+    $("#row-top").css("flex-direction", "column");
+    $("#row-top").css("align-items", "center");
+    $("#row-top").css("padding-bottom", "0");
+    $("#row-top").html("<div class='winner title'>Défaite</div>");
+    $("#row-top").append("<div class='winner'>(" + mot + ")</div>");
+
+    $(".key").css("opacity", "0.5");
+    $(".key").unbind("click");
+    $(".key").css("cursor", "not-allowed");
+}
 
 async function randomWord() {
     const jsonData = await fetch("./dico.json").then(res => res.json());
