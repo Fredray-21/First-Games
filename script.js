362c8dbc799d7d748c7e905c9e0e6fc35129e677
Unaty498 <50841236+Unaty498@users.noreply.github.com>
2022-04-24 19:37:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -76,14 +76,7 @@ async function randomWord() {
 
 
 function strNoAccent(a) {
-    let b = "áàâäãåçéèêëíïîìñóòôöõúùûüýÁÀÂÄÃÅÇÉÈÊËÍÏÎÌÑÓÒÔÖÕÚÙÛÜÝ",
-        c = "aaaaaaceeeeiiiinooooouuuuyAAAAAACEEEEIIIINOOOOOUUUUY",
-        d = "";
-    for (let i = 0, j = a.length; i < j; i++) {
-        let e = a.substr(i, 1);
-        d += (b.indexOf(e) !== -1) ? c.substr(b.indexOf(e), 1) : e;
-    }
-    return d;
+    return a.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
 
